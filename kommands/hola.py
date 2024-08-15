4d1e96b28285f6e5b55bf52b8b1ec1cbf4be1027
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-08-15 04:00:12 -0600

    Update hola.py

--- kommands/hola.py
+++ kommands/hola.py
@@ -1,6 +1,9 @@
-def hola(client, message, args):
-    chat = message.Info.MessageSource.Chat
-    client.reply_message(chat, "Hola mundo", message)
+from neonize.client import NewClient
+from neonize.events import MessageEv
 
 def register(commands):
-    commands["hola"] = hola
+    commands["hola"] = hola #Acá se define el comando, cambialo por el que tu quieras, el prefijo se añade automaticamente.
+
+def hola(client: NewClient, message: MessageEv, args):
+    chat = message.Info.MessageSource.Chat
+    client.send_message(chat, "Hola mundo") # Acá cambias lo que quieres que diga el bot
