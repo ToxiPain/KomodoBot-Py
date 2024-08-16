e9b7ef18c597cac5fe3606cf5221fc616e9a6e48
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-08-16 05:27:38 -0600

    Update mensaje.py

--- kommands/mensaje.py
+++ kommands/mensaje.py
@@ -1,11 +1,12 @@
+# Ejemplo de envio de mensaje simple. Edita el texto dentro de comillas.
 from neonize.client import NewClient
 from neonize.events import MessageEv
 
 def register(commands):
-    commands["hola"] = hola #Acá se define el comando, cambialo por el que tu quieras, el prefijo se añade automaticamente.
+    commands["hola"] = hola #Acá se define el comando, cambialo por el que tu quieras, el prefijo se añade automaticamente y puedes cambiarlo en "config.py" en la parte de prefix.
 
 def hola(client: NewClient, message: MessageEv, args):
     chat = message.Info.MessageSource.Chat
     client.send_message(chat, "Hola soy KomodoBot!") # Acá cambias lo que quieres que diga el bot
-# send_message - Para que simplemente envie texto Plano
-# reply_message - Puedes añadirlo para que conteste al mensaje del comando.
+    
+# Puedes cambiar client.send_message a client.reply_message para que pase de simplemente mandar texto plano a contestar el mensaje del comando.
