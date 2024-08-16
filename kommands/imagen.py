df757ef858d66088d4dc3abc82a58b7a14f4a373
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-08-16 05:36:25 -0600

    Update imagen.py

--- kommands/imagen.py
+++ kommands/imagen.py
@@ -2,7 +2,7 @@ from neonize.client import NewClient
 from neonize.events import MessageEv
 
 def register(commands):
-    commands["imagen"] = imagen
+    commands["imagen"] = imagen # Acá se define el comando, cambialo por el que tu quieras, el prefijo se añade automaticamente y puedes cambiarlo en "config.py" en la parte de prefix.
 
 def imagen(client: NewClient, message: MessageEv, args):
     chat = message.Info.MessageSource.Chat
