d08694b4d9fc30aea10de9ac64dc094c20f3afe9
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-08-16 04:33:38 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # Welcome to KomodoBot
 project_name = "KommodoBot-Py"
-project_version = "1.0.1"
+project_version = "1.0.2"
 developer = "Utvikler_Team"
+
+# Manejo de prefijos
+PREFIXES = ["/", ".", "#", "!"]
 
 #Manejo de ApiKeys: 
 groq_apikey = ["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"]
