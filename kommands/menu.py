20e81c646cdfc9eb10e6c2fe3eb2685e9d0a1602
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-08-20 22:44:03 -0600

    Create menu.py

---
+++ kommands/menu.py
@@ -0,0 +1,11 @@
+# Ejemplo de envio de mensaje simple. Edita el texto dentro de comillas.
+from neonize.client import NewClient
+from neonize.events import MessageEv
+
+def register(commands):
+    commands["menu"] = menu
+
+def menu(client: NewClient, message: MessageEv, args):
+    chat = message.Info.MessageSource.Chat
+    client.send_message(chat, "┌────︶.︶.︶.︶.︶.︶.︶.︶ ୭̥⋆*｡\n ੈ₊˚༅༴│↷◌⁺˖ *KomodoBot-Py 🌅*\n ੈ₊˚༅༴│↷◌⁺˖ *Versión:* 1.0.2\n\n*✎｡｡｡ Comandos de ejemplo:*\n⿻ /hola\n⿻ /ia\n⿻ /boton\n⿻ /copiar\n⿻ /imagen\n\n*✎｡｡｡ Comandos Generales:*\n⿻ /ping") 
+  
