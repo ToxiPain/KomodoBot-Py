54b91de0bb29a8edb4e5e6b486a8bee352754a6f
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-08-20 22:48:28 -0600

    Update menu.py

--- kommands/menu.py
+++ kommands/menu.py
@@ -6,5 +6,5 @@ def register(commands):
 
 def menu(client: NewClient, message: MessageEv, args):
     chat = message.Info.MessageSource.Chat
-    client.send_message(chat, "┌────︶.︶.︶.︶.︶.︶.︶.︶ ୭̥⋆*｡\n ੈ₊˚༅༴│↷◌⁺˖ *KomodoBot-Py 🌅*\n ੈ₊˚༅༴│↷◌⁺˖ *Versión:* 1.0.2\n\n*✎｡｡｡ Comandos de ejemplo:*\n⿻ /hola\n⿻ /ia\n⿻ /boton\n⿻ /copiar\n⿻ /imagen\n\n*✎｡｡｡ Comandos Generales:*\n⿻ /ping") 
+    client.send_message(chat, "┌────︶.︶.︶.︶.︶.︶.︶.︶ ୭̥⋆*｡\n ੈ₊˚༅༴│↷◌⁺˖ *KomodoBot-Py 🌅*\n ੈ₊˚༅༴│↷◌⁺˖ *Versión:* 1.0.2\n\n*✎｡｡｡ Comandos de ejemplo:*\n⿻ /hola\n⿻ /bot\n⿻ /boton\n⿻ /copiar\n⿻ /imagen\n\n*✎｡｡｡ Comandos Generales:*\n⿻ /ping\n⿻ /dado\n\nNuevas funciones generales se añadirán proximamente...") 
   
