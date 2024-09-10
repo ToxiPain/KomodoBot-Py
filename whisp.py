fe433076f6b57bee61d2a62d1f533b36a371d480
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-09-10 11:01:15 -0600

    Update whisp.py

--- whisp.py
+++ whisp.py
@@ -6,7 +6,7 @@ from neonize.events import MessageEv
 from neonize.utils.message import get_message_type
 import json
 import os
-import config  
+import config
 
 commands = {}
 komodo_key = None  
@@ -24,17 +24,18 @@ def build_komodo_key():
 complete_komodo_key = build_komodo_key()
 
 komodo_key_path = os.path.join('datamedia', 'komodokey.json')
+chat_states_path = os.path.join('datamedia', 'chat_states.json')
 
 # Guardar la KomodoKey en un archivo JSON
 def save_komodo_key(key):
-    os.makedirs(os.path.dirname(komodo_key_path), exist_ok=True) 
+    os.makedirs(os.path.dirname(komodo_key_path), exist_ok=True)
     with open(komodo_key_path, 'w') as file:
         json.dump({"komodo_key": key}, file)
 
 # Cargar la KomodoKey desde el archivo JSON
 def load_komodo_key():
     if not os.path.exists(komodo_key_path):
-        return None  
+        return None
 
     try:
         with open(komodo_key_path, 'r') as file:
@@ -46,6 +47,45 @@ def load_komodo_key():
     except Exception as e:
         logging.error(f"Error al cargar la KomodoKey: {e}")
         return None
+
+# Guardar el estado de los chats en el archivo JSON
+def save_chat_states():
+    os.makedirs(os.path.dirname(chat_states_path), exist_ok=True)
+    with open(chat_states_path, 'w') as file:
+        json.dump(chat_states, file)
+
+# Cargar el estado de los chats desde el archivo JSON
+def load_chat_states():
+    if not os.path.exists(chat_states_path):
+        return {}
+
+    try:
+        with open(chat_states_path, 'r') as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        logging.error("Error al leer el archivo JSON de estados de chat: contenido no válido.")
+        return {}
+    except Exception as e:
+        logging.error(f"Error al cargar el estado de los chats: {e}")
+        return {}
+
+chat_states = load_chat_states()
+
+# Comando para activar el bot en un chat específico
+def turn_on(client: NewClient, message: MessageEv, args, is_group: bool, sender: str):
+    chat = message.Info.MessageSource.Chat
+    chat_id = str(chat)  
+    chat_states[chat_id] = "on"  # Establece el estado del chat en "on"
+    save_chat_states() 
+    client.reply_message("El bot ha sido activado en este chat.", message)
+
+# Comando para desactivar el bot en un chat específico
+def turn_off(client: NewClient, message: MessageEv, args, is_group: bool, sender: str):
+    chat = message.Info.MessageSource.Chat
+    chat_id = str(chat)  # Convertir el chat a un string
+    chat_states[chat_id] = "off"  # Establece el estado del chat en "off"
+    save_chat_states()  # Guardar el estado de chat (actualizado)
+    client.reply_message("El bot ha sido desactivado en este chat.", message)
 
 # Cargar los comandos disponibles
 def load_commands():
@@ -64,23 +104,35 @@ def handler(client: NewClient, message: MessageEv):
     global komodo_key
     text = message.Message.conversation or message.Message.extendedTextMessage.text
     chat = message.Info.MessageSource.Chat
+    chat_id = str(chat)  # Convertir el chat a un string
     sender = message.Info.MessageSource.Sender.User  # Número de teléfono del remitente
+    is_group = message.Info.MessageSource.IsGroup  # Identificar si es enviado en un grupo
     msg_type = get_message_type(message)
     
-    # Detección de grupos o chats privados
-    is_group = message.Info.MessageSource.IsGroup
-    group_info = message.Info.MessageSource if is_group else None
+    # Procesar el comando ON/OFF
+    for prefix in config.PREFIXES:
+        if text.startswith(f"{prefix}on"):
+            turn_on(client, message, [], is_group, sender)
+            return
+        elif text.startswith(f"{prefix}off"):
+            turn_off(client, message, [], is_group, sender)
+            return
+
+    # Verificar el OFF
+    if chat_id in chat_states and chat_states[chat_id] == "off":
+        logging.info(f"El bot está desactivado en el chat: {chat_id}")
+        return  
 
     if komodo_key is None:
         komodo_key = load_komodo_key()
 
-    if text and text[0] in config.PREFIXES:  
+    if text and text[0] in config.PREFIXES:
         for prefix in config.PREFIXES:
             if text.startswith(f"{prefix}key"):
                 key = text.split(" ")[1]
                 if key == complete_komodo_key:
                     komodo_key = key
-                    save_komodo_key(komodo_key)  
+                    save_komodo_key(komodo_key)
                     client.reply_message("*KomodoKey definida correctamente! ✅*", message)
                 else:
                     client.reply_message("*KomodoKey incorrecta!!*", message)
@@ -90,14 +142,14 @@ def handler(client: NewClient, message: MessageEv):
             client.reply_message("*Por favor, define la KomodoKey con /key [tu_clave] para usar comandos.*\n\n*Puedes pedirla a:*\n*Mi creador:* wa.me/50557418454\n*Grupo Oficial:* https://chat.whatsapp.com/EnkVyluXN2rDtrCekiYxxD", message)
             return
 
-        command = text.split(" ")[0][1:]
+        command = text.split(" ")[0][1:]  
         args = text.split(" ")[1:]
 
         if command in commands:
-            commands[command](client, message, args, is_group, sender)  # Se pasa is_group y sender
-            config.commands_processed += 1  
+            commands[command](client, message, args, is_group, sender)  
+            config.commands_processed += 1
         else:
-            client.reply_message("Lo siento, comando /{command} no encontrado!", message)
+            client.reply_message(f"Lo siento, comando /{command} no encontrado!", message)
             logging.info(f"Comando no encontrado: {command}")
     else:
         logging.info(f"Mensaje recibido sin prefijo: {text}")
