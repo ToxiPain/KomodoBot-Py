97b37b5014ad4d338f1d5b220ad7e31f0b704c69
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-09-10 11:26:42 -0600

    Update ejemplo_imagen.py

--- kommands/ejemplo_imagen.py
+++ kommands/ejemplo_imagen.py
@@ -5,7 +5,7 @@ from neonize.events import MessageEv
 def register(commands):
     commands["imagen"] = imagen #Acá se define el comando, cambialo por el que tu quieras, el prefijo se añade automaticamente y puedes cambiarlo en "config.py" en la parte de prefix.
 
-def imagen(client: NewClient, message: MessageEv, args):
+def imagen(client: NewClient, message: MessageEv, args, is_group: bool, sender: str):
     chat = message.Info.MessageSource.Chat
     
     # Ruta de la imagen a enviar
