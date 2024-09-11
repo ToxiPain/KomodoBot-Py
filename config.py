b8232caedd509842b95e3ae912cfeb25eb75619c
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-09-10 22:19:17 -0600

    Lista de Owners

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ developer = "Utvikler_Team"
 
 # Manejo de prefijos
 PREFIXES = ["/", ".", "#", "!"]
+# Lista de números de Owners
+OWNERS = ["50557418454", "50585424403", "xxxxxxxxx", "xxxxxxxxx"]
 
 # Manejo de ApiKeys
 groq_apikey = ["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"]
