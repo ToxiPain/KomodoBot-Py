1c04ef23fdc41ffd92536681707e63ac15284af8
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2024-09-10 23:25:23 -0600

    Update menu.py

--- kommands/menu.py
+++ kommands/menu.py
@@ -3,7 +3,34 @@ from neonize.events import MessageEv
 
 def register(commands):
     commands["menu"] = menu
+    commands["help"] = menu
 
 def menu(client: NewClient, message: MessageEv, args, is_group: bool, sender: str):
     chat = message.Info.MessageSource.Chat
-    client.send_message(chat, "┌────︶.︶.︶.︶.︶.︶.︶.︶ ୭̥⋆*｡\n ੈ₊˚༅༴│↷◌⁺˖ *KomodoBot-Py 🌅*\n ੈ₊˚༅༴│↷◌⁺˖ *Versión:* 1.0.2\n ੈ₊˚༅༴╰────︶.︶ ⸙ ͛ ͎ ͛  ︶.︶ ੈ₊˚༅,\n\n✎｡｡｡ *Comandos de ejemplo:*\n⿻ /hola\n⿻ /bot\n⿻ /boton\n⿻ /copiar\n⿻ /imagen\n\n*✎｡｡｡ Comandos Generales:*\n⿻ /ping\n⿻ /dado\n\nNuevas funciones generales se añadirán proximamente...") 
+    push_name = message.Info.Pushname 
+
+    greeting = f"¡Hola, {push_name}! 👋\nEstos son los comandos disponibles\n\n"
+
+    menu_message = (
+        f"┌────︶.︶.︶.︶.︶.︶.︶.︶ ୭̥⋆*｡\n"
+        "✧ ₊ ˚ ✩ *KomodoBot-Py 🌅*\n"
+        "✧ ₊ ˚ ✩ *Versión:* 1.0.2\n"
+        f"✧ ₊ ˚ ✩ ╰────︶.︶ ⸙ ͛ ͎ ͛  ︶.︶ ✧ ₊ ˚ ✩,\n{greeting}"
+        "✎｡｡｡ *Comandos de ejemplo:*\n"
+        "⿻ /hola\n"
+        "⿻ /bot (texto)\n"
+        "⿻ /boton\n"
+        "⿻ /copiar\n"
+        "⿻ /imagen\n\n"
+        "*✎｡｡｡ Comandos Generales:*\n"
+        "⿻ /ping\n"
+        "⿻ /dado\n"
+        "⿻ /sticker\n"
+        "⿻ /on /off\n"
+        "⿻ /ban (@tag) /unban (@tag)\n"
+        "⿻ /seguir\n"
+        "⿻ /notify\n\n"                        
+        "Nuevas funciones generales se añadirán próximamente..."
+    )
+
+    client.send_message(chat, menu_message)
