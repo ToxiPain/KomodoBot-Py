07a69e598220d108ec5e92736d5df90db2799726
ToxiPain <109121365+ToxiPain@users.noreply.github.com>
2025-03-11 10:30:00 -0600

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ PREFIXES = ["/", ".", "#", "!"]
 OWNERS = ["50557418454", "50585424403", "xxxxxxxxx", "xxxxxxxxx"]
 
 # Manejo de ApiKeys
-groq_apikey = ["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"]
+groq_apikey = ["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"]
 
 # Contador global de comandos procesados
-commands_processed = 0
+commands_processed = 1
